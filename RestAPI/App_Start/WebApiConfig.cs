c7d0aad18ea7952844bc33eb24f10ca4fabdb513
Daniel Sroka <d.sroka@ela.pl>
2016-06-10 20:39:27 +0200

    enabling CORS

--- RestAPI/App_Start/WebApiConfig.cs
+++ RestAPI/App_Start/WebApiConfig.cs
@@ -7,11 +7,11 @@ namespace RestAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableCors();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new {id = RouteParameter.Optional}
-                );
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new {id = RouteParameter.Optional});
         }
     }
 }
